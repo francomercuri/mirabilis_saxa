afe713471b5d9a3875fd5941a37ea8259ecb5d5c
francomercuri <francomercuri@hotmail.com>
2022-02-03 11:38:18 -0300

    counter fixed

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -6,19 +6,16 @@ import Button from './Button'
 import Counter from './counter.js'
 
 
-function addToCart(a, b){
-    return (a + b)
-    
-}
+
 
 
 export default function ItemListContainer({greeting, initial, onAdd}){
-    const [count,setCount] = useState(0)
+    
     return(
             <div>
                 <h3>{greeting}</h3>
-                <Counter setCount={setCount} initial={count} stock={12} count={count} />
-                <Button onAdd= { addToCart(0, count) } />
+                <Counter  initial={0} stock={12} />
+                <Button />
             </div>
         );
     }
--- src/components/counter.js
+++ src/components/counter.js
@@ -3,13 +3,23 @@ import react, {useState} from 'react';
 
 
 
-export default function Counter({ initial, count, setCount, stock }){
-    
+export default function Counter({ initial, stock}){
+    const [count,setCount] = useState(initial);
+    function addItem(){
+        if(count < stock){
+        setCount ( count + 1 );
+        }
+    }
+    function substractItem(){
+        if(count > 0){
+            setCount ( count - 1 );
+        };
+    }
     return(
         <div>
-        <i className="fas fa-minus" onClick={() => {setCount( count -1 )}}></i>
-        <span>{initial}</span>
-        <i className="fas fa-plus" onClick={() => setCount( count +1 )}></i>
+        <i className="fas fa-minus" onClick={substractItem}></i>
+        <span>{count}</span>
+        <i className="fas fa-plus" onClick={addItem}></i>
         </div>
     );
 }
